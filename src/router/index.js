9bd8db03f43eec3354fed7024e20821283f4c096
BK1918 liyanyan <email@823629137.com>
2019-11-11 23:23:09 +0800

    11-11

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+const router=new VueRouter({
+    mode:"hash",
+    routes:[
+        {
+            path:"/",
+            redirect:""
+        },
+        
+    ]
+})
+export default router;
